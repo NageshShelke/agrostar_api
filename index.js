afdf5d1e1a710d29330770e3f9b52a5dc75203c8
nagesh <nagesh@quolam.com>
2025-12-10 11:10:48 +0530

    updated index.json

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ const products = require('./products_data.json');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Serve images from public folder
+
 app.use('/public', express.static('public'));
 
-// Routes
+app.get("/", (req, res) => {
+  return res.status(200).json({ status: "ok", message: "Agrostar API is up and running!" });
+});
+
+
 app.get("/products", (req, res) => {
   return res.json(products);
 });
 
 app.get("/products/:id", (req, res) => {
   const id = Number(req.params.id);
-  const product = products.agro_products.find((p) => p.id === id);
+  const product = products.find((p) => p.id === id);
   return res.json(product);
 });
 
