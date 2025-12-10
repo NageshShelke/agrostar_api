3126e9dbe6cf5d406c47b1a9bdb59f7197fef4fb
nagesh <nagesh@quolam.com>
2025-12-10 11:55:18 +0530

    allow all origin

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const products = require('./products_data.json');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-
+app.use(cors());
 app.use('/public', express.static('public'));
 
 app.get("/", (req, res) => {
